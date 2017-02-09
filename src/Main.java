0780e45fda273967d10a72d919213783a8ff4c75
Kayeng Thao <thaokaye@msoe.edu>
2017-02-09 17:58:33 -0600

    added prelims for the lols

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,7 @@
  * Created by thaokaye on 2/9/2017.
  */
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("What are you doing with your life?");
+    }
 }
